cb8e2a13a71fc7179d23f48b04c834bf91093f70
ZhaoqiGao1 <122407578+ZhaoqiGao1@users.noreply.github.com>
2024-02-21 15:29:38 -0500

    update qcomps

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,13 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import List from './qcomps/arrObj'
+import BucketList from "./qcomps/arrObj";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <BucketList />
     </div>
   )
 }
